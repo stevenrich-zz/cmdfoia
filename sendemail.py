1b6ab249ff1e4441a1bc315bc2a0731acf046f49
Steven Rich <steven.rich@washpost.com>
2016-01-25 18:19:28 -0500

    add section comments

--- sendemail.py
+++ sendemail.py
@@ -9,6 +9,7 @@ import datetime
 
 #I know it doesn't work just yet. I'm working backwards
 
+#user inputs
 recipient = raw_input("Email address you want to send your FOIA to: ")
 user = raw_input("Email username: ")
 password = getpass.getpass()
@@ -16,13 +17,16 @@ name = raw_input("Your name: ")
 organization = raw_input("Your organization: ")
 filename = "FOIA" + str(unique_id) + ".pdf"
 
+#connect to SMTP email server (just Outlook right now)
 smtpObj = smtplib.SMTP('m.outlook.com', 587)
 smtpObj.ehlo()
 smtpObj.starttls()
 smtpObj.login(user, password)
 
+#write email
 to = [str(recipient)]
 bcc = [user]
+tos = to + bcc
 fromAddr = user
 subject = "Open records request"
 text = "To whom it may concern:\n\nPlease find attached a request under public records law. If you have any questions or concerns, please email me at" + str(user) + ".\n\nThank you,\n\n" +str(name) + "\n" +str(organization) + "\n" + str(user)
@@ -34,13 +38,13 @@ emailMsg['To'] = ', '.join(to)
 emailMsg['Bcc'] = ", ".join(bcc)
 emailMsg.attach(body)
 
+#attach file
 fileMsg = MIMEBase('application','pdf')
 fileMsg.set_payload(file(str(filepath)).read())
 Encoders.encode_base64(fileMsg)
 fileMsg.add_header('Content-Disposition','attachment;filename=' + str(filename))
 emailMsg.attach(fileMsg)
 
-tos = to+bcc
+#send email and log out of SMTP server
 smtpObj.sendmail(fromAddr, tos, emailMsg.as_string())
-
 smtpObj.quit()
