f9e8496a2e1fb4e66cac3e01f1b16b3990b47d60
Steven Rich <steven.rich@washpost.com>
2016-02-06 16:21:50 -0500

    Update writefoia.py

--- writefoia.py
+++ writefoia.py
@@ -1,1 +1,16 @@
+import csv
+import re
+import string
+import sys
+import os
+import shutil
+from docx import Document
+from docx.shared import Inches
+from docx.shared import Pt
+from docx.enum.style import WD_STYLE
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+import datetime
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
